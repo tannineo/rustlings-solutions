f433bfbef80b7128a984c9be7ed4fe6f1e00dfa5
tannineo <tannineo@outlook.com>
2022-10-13 12:41:35 +0100

    complete macros

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,8 +1,6 @@
 // macros4.rs
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
